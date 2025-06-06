37894aa20ea2adb3396e2cfd8c606373ccc22bbf
Shilpa K <kshilpa053@gmail.com>
2025-06-06 14:49:31 +0530

    Added 1 more print statement

--- src/ex_01_Java_Basic/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorld.java
@@ -3,5 +3,6 @@ package ex_01_Java_Basic;
 public class Lab001_HelloWorld {
     public static void main(String[] agrs) {
         System.out.println("I am running Java Program");
+        System.out.println("commit the modification code");
     }
 }
